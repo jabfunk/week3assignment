089ad3dbcae50f1e0a6815f8e9437fa5dd740695
Julie Boehm <julie.anne.boehm@gmail.com>
2022-09-07 13:47:02 -0600

    commit

--- index.js
+++ index.js
@@ -25,17 +25,16 @@ const firstItem = names[0]
 console.log(firstItem)
 
 //#5 new array 
-/*let nameLengths = names.map(function(element) {
-    return element.length
-})
-console.log(nameLengths)
+let lengths = names.map(function(element) {
+    return element.length; 
+});
+console.log(lengths);
 
-
-let sum = namelengths.reduce(function(accumulator, currentValue) {
+let sum = lengths.reduce(function(accumulator, currentValue){
     return accumulator + currentValue; 
-}, 10); 
-console.log(sum)*/
-
+}); 
+console.log(sum);
+    
 // #7 Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’)
 function twoParameters(word, n) {
     var parameters = " ";
@@ -55,17 +54,41 @@ function fullName(first, last) {
 fullName("Julie", "Boehm")
 
 // #9 Function that takes array of numbers and returns true if sum > 100
-/*let numbers = [1, 3, 5, 9, 101];
+let numbers = [1, 3, 5, 9, 101];
 
-function trueHundred(numbers) {
-    
+let sumNumber = numbers.reduce(function(a, b) {
+    return a + b; 
+}, 0);
+console.log(sumNumber)
+
+function isHundred(sumNumber) {
+    if (sumNumber >= 100) {
+        return true
+    } else {
+        return false
+    }
 }
-*/
+
 
 // #10 function that takes array of numbers and returns average
+function ArrayAvg(myArray) {
+    var i = 0, summ = 0, ArrayLen = myArray.length;
+    while (i < ArrayLen) {
+        summ = summ + myArray[i++];
+}
+    return summ / ArrayLen;
+}
+var myArray = [1, 5, 2, 3, 7];
+var a = ArrayAvg(myArray);
+console.log(a)
 
 // #11 function takes two arrays of numbers and returns true if average in first array is greater than average in second array
+function twoArrays(array1, array2) {
+
+}
 
 // #12 function called willBuyDrink that takes a boolean isHotOutside and a number moneyInPocket and returns true if hot outside and if money is greater that 10.50
+
+
 
 // #13 function solves a problem - write what that function does 
